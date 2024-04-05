7f764314b4d5730786e3b1f7b80d6d900819f884
onelessangel <69592470+onelessangel@users.noreply.github.com>
2024-04-05 16:28:58 +0300

    Added secondary activity

---
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2SecondaryActivity.java
@@ -0,0 +1,17 @@
+package ro.pub.cs.systems.eim.colocviu1_2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Colocviu1_2SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
