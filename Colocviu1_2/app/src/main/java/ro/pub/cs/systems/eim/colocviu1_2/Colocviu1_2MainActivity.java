a35db8c1e51928e38e1c5504c19878a56ffdac12
onelessangel <69592470+onelessangel@users.noreply.github.com>
2024-04-05 16:49:21 +0300

    Added b2 and c1

--- Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
@@ -1,10 +1,15 @@
 package ro.pub.cs.systems.eim.colocviu1_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +18,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class Colocviu1_2MainActivity extends AppCompatActivity {
     Button addButton, computeButton;
     EditText nextTermEditText, allTermsEditText;
+    private ActivityResultLauncher<Intent> startActivityForResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,26 @@ public class Colocviu1_2MainActivity extends AppCompatActivity {
         nextTermEditText = findViewById(R.id.nextTermEditText);
         allTermsEditText = findViewById(R.id.allTermsEditText);
 
+        startActivityForResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent intent = result.getData();
+                if (result.getData() != null) {
+                    int sum;
+
+                    if (intent.hasExtra("SUM_VALUE")) {
+                        sum = intent.getIntExtra("SUM_VALUE", 0);
+//                        Log.d("DATA", String.valueOf(sum));
+                        Toast.makeText(getApplication(), String.valueOf(sum), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                Toast.makeText(getApplication(), "OK", Toast.LENGTH_SHORT).show();
+            } else if (result.getResultCode() == RESULT_CANCELED){
+                Toast.makeText(getApplication(), "CANCEL", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplication(), "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         addButton.setOnClickListener(view -> {
             int number = Integer.parseInt(nextTermEditText.getText().toString());
             String currTerms = allTermsEditText.getText().toString();
@@ -39,6 +65,14 @@ public class Colocviu1_2MainActivity extends AppCompatActivity {
             } else {
                 allTermsEditText.setText(allTermsEditText.getText().toString() + " + " + String.valueOf(number));
             }
+        });
+
+        computeButton.setOnClickListener(view -> {
+            String allTerms = allTermsEditText.getText().toString();
+
+            Intent intent = new Intent("ro.pub.cs.systems.eim.colocviu1_2.intent.action.Colocviu1_2SecondaryActivity");
+            intent.putExtra("ALL_TERMS", allTerms);
+            startActivityForResultLauncher.launch(intent);
         });
 
     }
--- Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2SecondaryActivity.java
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2SecondaryActivity.java
@@ -1,17 +1,48 @@
-package ro.pub.cs.systems.eim.colocviu1_2;
+    package ro.pub.cs.systems.eim.colocviu1_2;
 
-import android.os.Bundle;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-public class Colocviu1_2SecondaryActivity extends AppCompatActivity {
+    public class Colocviu1_2SecondaryActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Intent intent = getIntent();
+            if (intent != null) {
+                if (intent.hasExtra("ALL_TERMS")) {
+                    String sumValue = intent.getStringExtra("ALL_TERMS");
+                    int result = computeSum(sumValue);
+//                    Log.d("DATA", sumValue);
+//                    Log.d("DATA", String.valueOf(result));
+
+                    Intent resultIntent = new Intent(this, Colocviu1_2MainActivity.class);
+                    resultIntent.putExtra("SUM_VALUE", result);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            }
+        }
+
+        private int computeSum(String sumValue) {
+            if (sumValue == null) {
+                return -1;
+            }
+            String[] numbers = sumValue.split("\\+");
+            int sum = 0;
+
+            for (String num : numbers) {
+                sum += Integer.parseInt(num.trim());
+            }
+
+            return sum;
+        }
     }
-}
